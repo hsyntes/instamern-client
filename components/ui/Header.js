7720d6b73881f4ce462ea7e2b2ecdb4d68e4907b
hsyntes <se.hsyntes@gmail.com>
2024-06-30 21:39:20 +0300

    created modal bottom sheet component

--- components/ui/Header.js
+++ components/ui/Header.js
@@ -3,40 +3,49 @@ import Button from "./Button";
 import Image from "next/image";
 import { Charm_700 } from "../Layout";
 import MenuIcon from "./MenuIcon";
+import BottomSheet from "./modal/BottomSheet";
+import { useState } from "react";
 
 const Header = () => {
+  const [bottomSheet, setBottomSheet] = useState(false);
+
+  const handleBottomSheet = () => setBottomSheet(!bottomSheet);
+
   return (
-    <header className="grid grid-cols-12 items-center">
-      <section className="col-span-9 block lg:hidden">
-        <Link href={"/"} className="flex items-center gap-2">
-          <Image
-            src={"/logo.svg"}
-            width={96}
-            height={96}
-            className="w-6"
-            alt="Logo"
-          />
-          <h1
-            className={`text-3xl bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent ${Charm_700.className}`}
+    <>
+      <header className="grid grid-cols-12 items-center">
+        <section className="col-span-9 block lg:hidden">
+          <Link href={"/"} className="flex items-center gap-2">
+            <Image
+              src={"/logo.svg"}
+              width={96}
+              height={96}
+              className="w-6"
+              alt="Logo"
+            />
+            <h1
+              className={`text-3xl bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent ${Charm_700.className}`}
+            >
+              instamern
+            </h1>
+          </Link>
+        </section>
+        <section className="col-span-3 block lg:hidden">
+          <MenuIcon onClick={handleBottomSheet} />
+        </section>
+        <section className="col-span-3 lg:col-span-12 ms-auto hidden lg:block">
+          <Button
+            type={"button"}
+            variant={"primary"}
+            className={"rounded-xl ms-auto"}
+            onClick={() => router.push("/auth/signup")}
           >
-            instamern
-          </h1>
-        </Link>
-      </section>
-      <section className="col-span-3 block lg:hidden">
-        <MenuIcon />
-      </section>
-      <section className="col-span-3 lg:col-span-12 ms-auto hidden lg:block">
-        <Button
-          type={"button"}
-          variant={"primary"}
-          className={"rounded-xl ms-auto"}
-          onClick={() => router.push("/auth/signup")}
-        >
-          Sign up
-        </Button>
-      </section>
-    </header>
+            Sign up
+          </Button>
+        </section>
+      </header>
+      <BottomSheet show={bottomSheet} handleBottomSheet={handleBottomSheet} />
+    </>
   );
 };
 
--- components/ui/MenuIcon.js
+++ components/ui/MenuIcon.js
@@ -1,5 +1,5 @@
 const MenuIcon = ({ onClick }) => (
-  <span onClick={onClick}>
+  <span className="active:opacity-20 transition-all" onClick={onClick}>
     <div className="bg-secondary h-0.5 w-8 mb-1 ms-auto" />
     <div className="bg-secondary h-0.5 w-6 mb-1 ms-auto" />
     <div className="bg-secondary h-0.5 w-4 ms-auto" />
